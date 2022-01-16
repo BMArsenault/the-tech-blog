720953454353fd59a1ca398baaba5ba5177e730e
Brian <BMArsenault@gmail.com>
2022-01-16 03:23:54 -0700

    Cleaned up models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Post = require("./Post");
-const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 // create associations
@@ -32,14 +31,6 @@ Vote.belongsTo(Post, {
   foreignKey: 'post_id'
 });
 
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
-
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
@@ -56,4 +47,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
